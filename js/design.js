58f7a6aa8fc11bdd17088c87e8ad0a1aaf19bd9d
Eshan Singh Kachwaha <singheshan31@gmail.com>
2018-05-05 01:55:39 +0530

    final changes

--- js/design.js
+++ js/design.js
@@ -1,12 +1,12 @@
-document.body.onLoad = initialize();
+document.body.onLoad = initialize();//when the page loads
 var cards = document.getElementsByClassName("card"); //to store the cards
 var p=0; //to keep a track of opended cards
-let moves=0;
-var time="00:00:00";
+let moves=0; //inital moves
+var time="00:00:00"; //inital time
 var interval;
-var finalTime;
-var finalMoves;
-var finalStars;
+var finalTime; //for modal
+var finalMoves; //for modal
+var finalStars; //for modal
 var modal = document.getElementById("myModal");
 var star = document.querySelector(".stars");
 let movcount = document.getElementById("moves");
@@ -14,21 +14,22 @@ let timecount = document.getElementById("timer");
 movcount.innerHTML=moves;
 timecount.innerHTML=time;
 var starset = document.getElementsByClassName('fa-star');
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let seconds = 0; //inital time
+let minutes = 0; //initial time
+let hours = 0; //initial time
+
 //to open the cards
 for(let i=0;i<cards.length; i++){
 cards[i].addEventListener('click',function(){
   p++;
   cards[i].classList.add("open");
-  if(p===2){moves++; if(moves==12){starset[2].classList.add("star-change");} if(moves==18){starset[1].classList.add("star-change");} if(moves==25){starset[0].classList.add("star-change");} movcount.innerHTML = moves;
+  if(p===2){moves++; if(moves==12){starset[2].classList.add("star-change");} if(moves==18){starset[1].classList.add("star-change");} if(moves==25){starset[0].classList.add("star-change");} movcount.innerHTML = moves; //show the moves on the page
             let opened = document.getElementsByClassName("open"); //the opened cards
-              if(opened[0].children[0].getAttribute('class') == opened[1].children[0].getAttribute('class')){p=0; opened[0].classList.add("match"); opened[1].classList.add("match"); opened[1].classList.remove("open"); opened[0].classList.remove("open");} //if both cards are same
-            else{p=0; opened[0].classList.add("wrong-match"); opened[1].classList.add("wrong-match");
+              if(opened[0].children[0].getAttribute('class') == opened[1].children[0].getAttribute('class')){p=0; opened[0].classList.add("match"); opened[1].classList.add("match"); opened[1].classList.remove("open"); opened[0].classList.remove("open");}//if cards match
+              else{p=0; opened[0].classList.add("wrong-match"); opened[1].classList.add("wrong-match");//if cards dont match
             window.setTimeout(function(){opened[1].classList.remove("wrong-match","open"); opened[0].classList.remove("wrong-match","open");},800)}
             }
-   var matched = document.getElementsByClassName("match"); if(matched.length==16){finalResult(); }
+   var matched = document.getElementsByClassName("match"); if(matched.length==16){finalResult(); }//when all the cards are opened
 });
 }
 
@@ -47,7 +48,7 @@ function initialize(){
 	li_element.appendChild(item_element);
 	deck_begin.appendChild(li_element);}
   start_timer();
-};
+}
 
 //shuffling code taken from w3resource.com
 function shuffle(arra1) {
@@ -60,8 +61,9 @@ function shuffle(arra1) {
         arra1[index] = temp;
     }
     return arra1;
-};
+}
 
+//to star the stopwatch timer
 function start_timer(){
 interval = setInterval(function(){seconds++;
  if(seconds == 60){
@@ -79,8 +81,9 @@ interval = setInterval(function(){seconds++;
         else if(seconds>9){
           timecount.innerHTML = "0"+hours + ":0" + minutes + ":" + seconds;}
         else{timecount.innerHTML = "0"+hours + ":0" + minutes + ":0" + seconds;}
-      },1000);};
+      },1000);}
 
+//for displaying the modal
 function finalResult()
 {   clearInterval(interval);
     finalTime = timer.innerHTML;
@@ -89,7 +92,7 @@ function finalResult()
     document.querySelector(".moves-input").innerHTML = finalMoves;
     document.querySelector(".star-input").innerHTML = finalStars;
     document.querySelector(".time-input").innerHTML = finalTime;
-    modal.classList.add("show");
-  };
+    modal.classList.add("show"); //show the modal
+  }
 
-function closeModal(){modal.classList.remove("show");}
+function closeModal(){modal.classList.remove("show");} //close the modal
